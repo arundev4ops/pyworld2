f939dd95600bd0d7989cc5516669eb6936b7a6c7
Arunkumar Ramesha <rarunkumar@TOSHIBA-TSIP.COM>
2019-11-11 21:48:49 +0530

    Initial commit

---
+++ varbl.py
@@ -0,0 +1,14 @@
+v_name = "Arun"
+v_age = "34"
+v_native = "Mysuru"
+v_work_loc = "Bengaluru"
+v_str = "This is variable value"
+
+print(v_name + " is "+v_age+" years ")
+print(v_name + " is from "+v_native)
+print(v_name+" works in  "+v_work_loc)
+print(v_str.upper())
+print(v_str.upper().isupper())
+print(" Length is  "+str(len(v_str)))
+print(" v_str[0] "+v_str[0])
+print(v_str.index("is"))
